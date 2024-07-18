6408d83af2e580906aba6afa64b6af5a24cb6e52
Emanuel Martinez <emanuel.martinez@kandasoft.com>
2024-07-18 14:33:40 -0600

    Implemented enum for the period of time selector from the currency price page, deleted the target files that were still in the repo

--- src/test/java/forms/pages/CurrencyPricePage.java
+++ src/test/java/forms/pages/CurrencyPricePage.java
@@ -3,6 +3,7 @@ package forms.pages;
 import aquality.selenium.elements.interfaces.IButton;
 import aquality.selenium.elements.interfaces.ILabel;
 import aquality.selenium.forms.Form;
+import enums.PeriodOfTime;
 import org.openqa.selenium.By;
 import utilities.CurrenciesUtils;
 
@@ -19,27 +20,20 @@ public class CurrencyPricePage extends Form {
     String timeSelectorXPath = "//span[contains(@title, '%s')]";
 
 
-    public void clickTimeSelector(String time) {
-        String formattedXPath;
-        switch (time) {
-            case "24H":
-                formattedXPath = String.format(timeSelectorXPath, "24H");
+    public void clickTimeSelector(PeriodOfTime time) {
+        PeriodOfTime selectedOption = null;
+        for (PeriodOfTime option : PeriodOfTime.values()) {
+            if (option.getValue().equals(time.getValue())) {
+                selectedOption = option;
                 break;
-            case "1W":
-                formattedXPath = String.format(timeSelectorXPath, "1W");
-                break;
-            case "1M":
-                formattedXPath = String.format(timeSelectorXPath, "1M");
-                break;
-            case "1Y":
-                formattedXPath = String.format(timeSelectorXPath, "1Y");
-                break;
-            case "5Y":
-                formattedXPath = String.format(timeSelectorXPath, "5Y");
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid time option: " + time);
+            }
         }
+
+        if (selectedOption == null) {
+            throw new IllegalArgumentException("Invalid time option: " + time);
+        }
+
+        String formattedXPath = String.format(timeSelectorXPath, selectedOption.getValue());
         getElementFactory().getLabel(By.xpath(formattedXPath), "Time selector button").click();
     }
 
